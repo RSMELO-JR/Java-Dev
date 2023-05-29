5a03051aac86d2c1197eb4c56e2f3d6a75936a31
rsmelo-jr <rsmelojr@yahoo.com>
2023-05-29 12:27:51 -0300

    add new file: ExpresAritmeticas in EstrutSequencial

---
+++ EstrutSequencial/src/ExpresAritmeticas.java
@@ -0,0 +1,15 @@
+public class ExpresAritmeticas {
+    /* OPERADORES ARITMETICOS
+
+    Exemplos:
+        2 + 3 * 6 = 20;
+        2 * 6 / 3 = 04;
+        (3 + 2) * 4 = 20;
+        60 / (3 + 2) * 4 = 48;
+        60 / ((3 + 2) * 4) = 03;
+
+        14 % 3 = 02 > 14/3=4 sobram 2: MOD 02;
+        19 % 5 = 04 > 19/5=3 sobram 4: MOD 04;
+
+     */
+}
