057bcbaf8aaef27f76e2cdaca13116592aaf4775
rsmelo-jr <rsmelojr@yahoo.com>
2023-06-14 06:07:05 -0300

    add news files: Comandos de atribuição e Casting

--- EstrutSequencial/src/_7_ProcesDados.java
+++ EstrutSequencial/src/_7_ProcesDados.java
@@ -11,7 +11,7 @@ Exemplo 01:
         System.out.println(y);
         System.out.println();
 
-// Exemplo 02:
+// Exemplo 02: double
         int a;
         double b;
         a = 5;
@@ -26,6 +26,26 @@ Exemplo 01:
         h = 5.0;
         area = (b2 + B) / 2.0 * h;
         System.out.println(area);
+
+// Exemplo 04: Casting - Conversão explicita dos valores
+        int a2, b3;
+        a2 = 5;
+        b3 = 2;
+        double resultado;
+    //para que o programa retorne os todos os valores da operação, é preciso colocar a palavra "(double)" na frente da operação:
+        resultado = (double) a2 / b3;
+        System.out.println(resultado);
+
+// Exemplo 05:
+        double c;
+        int d;
+
+        c = 5.0;
+        d = a;
+        //erro que informa que vc não pode converter de 'double' para 'int';
+        //para realizar esta operação é preciso usar o casting:
+        d = (int) c;
+        System.out.println(c);
 
     }
 }
