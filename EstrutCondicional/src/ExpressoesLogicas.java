66ac4d8dc6bb1bbdb1ef67d92bbd8de06e5a7175
rsmelo-jr <rsmelojr@yahoo.com>
2023-06-14 12:54:45 -0300

    Add new file: Expressões Lógicas - Operadores && / || / !

---
+++ EstrutCondicional/src/ExpressoesLogicas.java
@@ -0,0 +1,33 @@
+public class ExpressoesLogicas {
+   public static void main(String[] args) {
+/* OPERADORES LÓGICOS
+Expressões Lógicas são também expressões que quando avaliadas, retornam um valor 'VERDADE', ou seja, Verdadeiro ou Falso.
+   && E;
+   || OU;
+   ! NÃO
+
+Operador && (E): A ideia por trás do operador E, é que TODAS AS CONDIÇÕES DEVEM SER VERDADEIRAS.
+Exemplo:
+   suponha x = 5; então:
+   x <= 20 && x ==10 = Resultado F.
+   x > 0 && x != 3 = Resultado V.
+   x <= 20 && x == 10 && x!= 3 = Resultado F.
+
+Operador || (OU): A ideia por trás desse operador, é PELO MENOS UMA DAS CONDIÇÕES, deve ser verdadeira.
+Exemplo:
+   suponha x = 5; então:
+   x == 10 || x <= 20 = Resultado V;
+   x > 0 || x != 3 = Resultado V;
+   x <= 0 || x != 3 || x != 5 = Resultado V.
+
+Operador NAO: A ideia por trás desse operador, é que ele INVERTE A CONDIÇÃO;
+Exemplo:
+   "Vc tem direito a uma bolsa de Estudos se NÃO POSSUIR uma renda maior que $3.000,;
+   João = R$5.000, ::: José = R$2.000,  Resultado = O JOSE ganha a bolsa de estudos.
+
+   ! (x==10) = Resultado V;
+   ! (x >= 2) = Resultado F;
+   ! (x<=20 && x==10) = V;
+ */
+   }
+}
