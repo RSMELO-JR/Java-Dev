9e0958bc3e21601df2cf7cf0ea9018d845fdf038
rsmelo-jr <rsmelojr@yahoo.com>
2023-06-14 12:21:14 -0300

    Add new file: Expressões Comparativas e Operadores Lógicos

---
+++ EstrutCondicional/src/ExpressoesComparativas.java
@@ -0,0 +1,12 @@
+public class ExpressoesComparativas {
+   public static void main(String[] args) {
+
+/* OPERADORES COMPARATIVOS
+São expressões que comparam expressões entre si, onde o resultado é um valor 'VERDADE', ou seha, Verdadeiro ou Falso.
+Por exemplo: 5 > 10 = falso.
+    > maior; < menor;
+   >= maior ou igual; <=menor ou igual;
+   == (= =)igual; !=(! =)diferente;
+*/
+   }
+}
