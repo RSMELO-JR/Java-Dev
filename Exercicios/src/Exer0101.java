7b677ef29242730a8ccfede18e2a25993aeac16d
rsmelo-jr <rsmelojr@yahoo.com>
2023-06-16 09:14:25 -0300

    add new files: Exercicios

---
+++ Exercicios/src/Exer0101.java
@@ -0,0 +1,22 @@
+/*
+Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa, conforme exewmplos:
+Entrada 10, 30; Saida 40;
+Entrada -35, 15; Saida -20;
+ */
+public class Exer0101 {
+   public static void main(String[] args) {
+      int x1, x2, x3, x4, soma1, soma2;
+      x1 = 10;
+      x2 = 30;
+      x3 = -35;
+      x4 = 15;
+
+      soma1 = x1 + x2;
+      soma2 = x3 + x4;
+
+      System.out.println(soma1);
+      System.out.println(soma2);
+
+   }
+}
---
+++ Exercicios/src/Exer0102.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+/*
+Faça um programa para ler o valor do raio de um círculo e depois,
+mostrar o valor da área deste círculo com quatro casas decimais conforme exemplos.
+Fórmula da área: area = π . raio2; Considere o valor de π = 3.14159.
+Entrada 2.00; Saida A=12.5664;
+Entrada 100.64; Saida A=31819.3103;
+Entrada 150.00; Saida A=70685.7750;
+ */
+public class Exer0102 {
+   public static void main(String[] args) {
+      Scanner PY = new Scanner(System.in);
+      double R, A, pi=3.14159;
+      R = PY.nextDouble();
+      A = pi * R * R;
+      System.out.printf("A=%.4f%n", A);
+      PY.close();
+   }
+}
